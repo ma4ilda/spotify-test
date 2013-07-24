49da1e682f18f2cd774354dd8e43e9e9efe1b489
Anastasiia Pukhyr <gyul4itay@gmail.com>
2013-07-24 22:40:44 +0300

    Change made to package structure

---
+++ src/test/java/spotify/sikuli/Patterns.java
@@ -0,0 +1,17 @@
+package spotify.sikuli;
+import java.net.URL;
+
+
+public class Patterns {
+	
+	static final URL Logo = Patterns.class.getResource("./logo.png");
+	static final URL LoginFailedImage = Patterns.class.getResource("./login_failed.png");
+	static final URL LoginFailedImage_Windows = Patterns.class.getResource("./login_failed_Windows.png");
+	static final URL LoginButtonImage = Patterns.class.getResource("./login_button.png");
+	static final URL PlayButtonImage = Patterns.class.getResource("./play_button.png");
+	static final URL SearchFieldImage = Patterns.class.getResource("./search.png");
+	static final URL PauseButtonImage = Patterns.class.getResource("./pause_button.png");
+	static final URL EmptySearchImage = Patterns.class.getResource("./empty_search.png");
+	static final URL StarIcon = Patterns.class.getResource("./star_and_share.png");
+
+}
